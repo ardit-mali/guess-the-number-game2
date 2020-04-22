1447b7b21bf06a6cd85ee29c106282100eb57619
Ardit Mali <54575263+ardit-mali@users.noreply.github.com>
2020-04-22 13:20:57 +0200

    Initial commit

---
+++ core/src/main/java/academy/learnprogramming/Main.java
@@ -0,0 +1,15 @@
+package academy.learnprogramming;
+
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+
+public class Main {
+
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("Guess the number game");
+
+    }
+
+}
